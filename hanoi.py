11662cdf29e5fe18b6ee913865eb9ab4ef67cc8b
eytanohana <eohana98@gmail.com>
2020-12-29 18:56:23 +0200

    add hanoi

---
+++ hanoi.py
@@ -0,0 +1,14 @@
+
+def hanoi(disc, from_, to):
+	def move(d, f, t, v):
+		if d <= 1:
+			print(f'Move disc {d} from {f} to {t}.')
+		else:
+			move(d-1, f, v, t)
+			print(f'Move disc {d} from {f} to {t}')
+			move(d-1, v, t, f)
+
+	move(disc, from_, to, 2)
+
+if __name__ == '__main__':
+	hanoi(3, 1, 3)		
