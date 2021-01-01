581d6b9a5ce42e2076985aad7d730c4baaf27093
eytanohana <eohana98@gmail.com>
2021-01-01 23:04:37 +0200

    turn into generator

--- hanoi.py
+++ hanoi.py
@@ -1,14 +1,16 @@
 
-def hanoi(disc, from_, to):
-	def move(d, f, t, v):
-		if d <= 1:
-			print(f'Move disc {d} from {f} to {t}.')
-		else:
-			move(d-1, f, v, t)
-			print(f'Move disc {d} from {f} to {t}')
-			move(d-1, v, t, f)
+def hanoi(disc):
+    def move(d, f, t, v):
+        if d <= 1:
+            yield f'Move disc {d} from {f} to {t}.'
+        else:
+            yield from move(d - 1, f, v, t)
+            yield f'Move disc {d} from {f} to {t}.'
+            yield from move(d - 1, v, t, f)
 
-	move(disc, from_, to, 2)
+    return move(disc, 1, 3, 2)
+
 
 if __name__ == '__main__':
-	hanoi(3, 1, 3)
+    for m in hanoi(20):
+        print(m)
